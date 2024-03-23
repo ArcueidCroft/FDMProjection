179dc43f5f810255a04938a93d98f7faf03abfd5
Arcueid <zqy_edu@163.com>
2024-03-23 11:41:29 +0800

    2024.3.23-2

--- src/Mainf.jl
+++ src/Mainf.jl
@@ -5,6 +5,7 @@
 
 using LinearAlgebra
 using SparseArrays
+import Plots as plt
 
 force(x, t) = 1
 
@@ -18,7 +19,7 @@ function Mainf()
     u = zeros(nt, nx)
 
     # * 边界控制和系数计算
-    row_Q = nx-bwidth
+    row_Q = nx-2*bwidth
     p = ht^2/(2*hx^4)
     
     # TODO 边界条件
@@ -26,15 +27,19 @@ function Mainf()
     # * 计算u
     for k = 2:nt-1
         # * 组装Q矩阵
+        # ** 首先组装非边界
+        # ** 对bwidth单独组装
         Q = sparse(zeros(row_Q, row_Q))
-        for row = 3:row_Q
+        for row = 3:row_Q-2
             Q[row, row+2] = -p
             Q[row, row+1] = 4*p
             Q[row, row]   = (1+ht)-6*p
             Q[row, row-1] = 4*p
             Q[row, row-2] = -p
         end
-
+        Q[1:2, 1:5] = Q[3:4, 3:3+5-1]
+        Q[row_Q-1:row_Q, row_Q-1-2:end] = Q[3:4, 1:4]
+        
         # * 组装f向量
         fk = zeros(row_Q)
         for row = 1:row
@@ -42,7 +47,7 @@ function Mainf()
         end
 
         # * 组装右端uk
-        # ** 首先组装Qt矩阵
+        # ** 首先组装Qt矩阵，Qt矩阵组装思路和Q相同
         # ** 再生成uk
         Qt = sparse(zeros(row_Q, row_Q))
         for row = 3:row_Q
@@ -52,12 +57,16 @@ function Mainf()
             Qt[row, row-1] = -4p
             Qt[row, row-2] = p
         end
-        uk = Qt*u[nt, :]
+        Qt[1:2, 1:5] = Qt[3:4, 3:3+5-1]
+        Qt[row_Q-1:row_Q, row_Q-1-2:end] = Qt[3:4, 1:4]
+        uk = Qt*u[nt, 3:end-2]
 
         # * 组装ukk:=u^{k-1}_{j}
-        ukk = u[nt-1, :]
+        ukk = u[nt-1, 3:end-2]
 
         # * 求解生成u^{k+1}
         u[nt+1, :] = Q\(fk+uk+ukk)
     end
+
+    plt.heatmap(u)
 end
